9b1ff0978f40b510796ae96bb020508c422872a2
Matias Mir <matiasmir.arg@gmail.com>
2020-09-16 17:38:07 -0300

    Added cfg for typescript and support for es6/es7

---
+++ src/loaders/routeLoader.ts
@@ -0,0 +1,14 @@
+import Routes from "../routes";
+
+export default function routeLoader(app) {
+  Routes.forEach((route) => {
+    app[route.method](route.route, (req, res, next) => {
+      const result = new route.controller()[route.action](req, res, next);
+      if (result instanceof Promise) {
+        result.then((result) => (result ? res.json(result) : undefined));
+      } else if (result) {
+        res.json(...result);
+      }
+    });
+  });
+}
